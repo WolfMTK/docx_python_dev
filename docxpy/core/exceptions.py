e2b7b95dc9e1054423aed772790df544808c724f
Alexander <qrer71@yandex.ru>
2023-08-09 17:23:20 +0300

    Create custom exception classes

---
+++ docxpy/core/exceptions.py
@@ -0,0 +1,9 @@
+class ErrorPathToDocx(Exception):
+    """
+    Class indicating an error in
+    the path to a file with docx extension.
+    """
+
+
+class ErrorDocxFile(Exception):
+    """Class indicating an error related to docx extension."""
