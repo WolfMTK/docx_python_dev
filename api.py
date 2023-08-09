5bbdcfa568f91f1165b94e9dd44b55ac0862db76
Alexander <qrer71@yandex.ru>
2023-08-09 17:22:28 +0300

    Delete file

--- api.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-
-def document(docx_file: str | None = None):
-    ...
--- services.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-from pathlib import Path
-
-
-def default_docx_path() -> Path:
-    return Path(__file__) / 'templates' / 'default.docx'
